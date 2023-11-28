b2eae62f0adc5668c8010da372d1304e8907c7ef
Ron <sumseq@gmail.com>
2023-11-27 16:40:58 -0800

    cc

--- build_examples/build_gcc_cpu.sh
+++ build_examples/build_gcc_cpu.sh
@@ -24,7 +24,7 @@ HDF5_LIB_FLAGS="-lhdf5_fortran -lhdf5hl_fortran -lhdf5 -lhdf5_hl"
 # Please set the compile flags based on your compiler and hardware setup.
 ###########################################################################
 
-FFLAGS="-O3 -march=native -fopenmp -ftree-parallelize-loops=${OMP_NUM_THREADS}"
+FFLAGS="-O3 -march=native -ftree-parallelize-loops=${OMP_NUM_THREADS}"
 
 ###########################################################################
 # Specify src filename:  Use hipft_gcc.f90 for GCC, otherwise use hipft.f90
--- build_examples/build_nvidia_cpu.sh
+++ build_examples/build_nvidia_cpu.sh
@@ -24,7 +24,7 @@ HDF5_LIB_FLAGS="-lhdf5_fortran -lhdf5hl_fortran -lhdf5 -lhdf5_hl"
 # Please set the compile flags based on your compiler and hardware setup.
 ###########################################################################
 
-FFLAGS="-O3 -march=native -mp=multicore -stdpar=multicore"
+FFLAGS="-O3 -march=native -stdpar=multicore"
 
 ###########################################################################
 # Specify src filename:  Use hipft_gcc.f90 for GCC, otherwise use hipft.f90
