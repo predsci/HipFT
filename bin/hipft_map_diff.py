73d7bae7576201de752ccca25b1bd6af08c914b5
Ron <sumseq@gmail.com>
2024-07-15 13:31:31 -0700

    MS: Updated map diff tool to allow diff hdf5 output and cutting the domain of diff near the poles.

--- bin/hipft_map_diff.py
+++ bin/hipft_map_diff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 import argparse
-from psihdf import rdh5
+from psihdf import rdh5, wrh5
 
 # Get rid of warning when nan or zero value in divisor
 np.seterr(divide='ignore', invalid='ignore')
@@ -17,6 +17,26 @@ def argParsing():
     parser.add_argument('file2',
                         type=str,
                         help="Second h5 to compare (reference).")
+
+    parser.add_argument('-diff',
+                        type=str,
+                        help="Create diff h5 file of the two input files.")
+
+    parser.add_argument('-dtn',
+                        type=str,
+                        help="Values of theta above this will be ignored. Can be float or division i.e. [np.pi/100]")
+
+    parser.add_argument('-dts',
+                        type=str,
+                        help="Values of theta below this will be ignored. Can be float or division i.e. [np.pi/100]")
+
+    parser.add_argument('-idtn',
+                        type=int,
+                        help="Values of theta above this will be ignored.")
+
+    parser.add_argument('-idts',
+                        type=int,
+                        help="Values of theta below this will be ignored.")
 
     return parser.parse_args()
 
@@ -55,12 +75,42 @@ def getdiff_maxape(x, y):
         d = -1
     return d
 
+
 # End of comparison functions ---------------------------------------------------------------
 
 args = argParsing()
 
-_,_,_,fieldData1 = rdh5(args.file1)
-_,_,_,fieldData2 = rdh5(args.file2)
+t1,t2,t3,fieldData1 = rdh5(args.file1)
+_,_,_,fieldData2    = rdh5(args.file2)
+
+fieldData1=np.array(fieldData1)
+fieldData2=np.array(fieldData2)
+
+ibot = 0;
+itop = -1;
+
+if (args.idts):
+    ibot = args.idts
+    
+if (args.idtn):
+    itop = len(t2)-args.idtn
+
+if (args.dts):
+    dts = eval(args.dts)
+    ibot = (np.abs(t2 - dts)).argmin()
+    if t2[ibot] < dts:
+        ibot+=1
+    
+if (args.dtn):
+    dtn = eval(args.dtn)
+    itop = (np.abs(t2 - dtn)).argmin()
+    if t2[itop] > dtn:
+        itop-=1
+
+if (ibot != 0 or itop != -1):
+    fieldData1 = fieldData1[ibot:itop+1,:]
+    fieldData2 = fieldData2[ibot:itop+1,:]
+    t2 = t2[ibot:itop+1]
 
 listOfdiff = []
 listOfdiff.append(getdiff_rmsd(fieldData1, fieldData2))
@@ -68,6 +118,15 @@ listOfdiff.append(getdiff_maxabs(fieldData1, fieldData2))
 listOfdiff.append(getdiff_mcvrmsd(fieldData1, fieldData2))
 listOfdiff.append(getdiff_mape(fieldData1, fieldData2))
 listOfdiff.append(getdiff_maxape(fieldData1, fieldData2))
+
+if (args.diff is not None):
+    fieldDataDiff = fieldData1 - fieldData2
+    if not args.diff.endswith('.h5'):
+        diff_file = args.diff+'.h5'
+    else:
+        diff_file = args.diff
+
+    wrh5(diff_file,t1,t2,t3,fieldDataDiff)
 
 out = ""
 
